6ca5b0f63f11c9a00a7c17fcd5a36c29c5a85fce
Álvaro Reneses <alvaroreneses@gmail.com>
2015-08-06 19:07:48 +0200

    New client included in the server

---
+++ src/main/java/com/matecat/converter/server/resources/ClientResource.java
@@ -0,0 +1,41 @@
+package com.matecat.converter.server.resources;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+
+/**
+ * Client resource
+ */
+@Path("/")
+public class ClientResource {
+
+    private static final String CLIENT_HTML_PATH = "/client/client.html";
+    private static final String CLIENT_HTML;
+    static {
+        try {
+            CLIENT_HTML = IOUtils.toString(ClientResource.class.getResourceAsStream(CLIENT_HTML_PATH), "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException("The client.html was not found");
+        }
+    }
+
+    /**
+     * Output the client
+     * @return Client
+     */
+    @GET
+    public Response client(@Context HttpServletRequest request) throws IOException {
+        return Response
+                .status(Response.Status.OK)
+                .entity(CLIENT_HTML)
+                .build();
+    }
+
+}
--- src/main/java/com/matecat/converter/server/resources/ConvertToXliffResource.java
+++ src/main/java/com/matecat/converter/server/resources/ConvertToXliffResource.java
@@ -12,7 +12,9 @@ import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
