b2ddb8151f79e4aba31e39a4a297bc8b51eee997
Giuseppe Silvano <giuseppe.silvano@gmail.com>
2016-09-14 16:19:53 +0200

    Add default filter and log messages

--- filters/src/main/java/com/matecat/filters/basefilters/FiltersRouter.java
+++ filters/src/main/java/com/matecat/filters/basefilters/FiltersRouter.java
@@ -2,6 +2,8 @@ package com.matecat.filters.basefilters;
 
 import com.matecat.converter.core.XliffProcessor;
 import com.matecat.converter.core.util.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -9,6 +11,8 @@ import java.util.List;
 import java.util.Locale;
 
 public class FiltersRouter {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(FiltersRouter.class);
 
     private List<IFilter> filters;
 
@@ -26,6 +30,9 @@ public class FiltersRouter {
     public File extract(File sourceFile, Locale sourceLanguage, Locale targetLanguage, String segmentation) {
         for (IFilter filter : filters) {
             if (filter.isSupported(sourceFile)) {
+                if (!(filter instanceof DefaultFilter)) {
+                    LOGGER.info("Using custom filter: " + filter.getClass().getCanonicalName());
+                }
                 return filter.extract(sourceFile, sourceLanguage, targetLanguage, segmentation);
             }
         }
@@ -35,9 +42,17 @@ public class FiltersRouter {
     public File merge(File xliff) {
         XliffProcessor processor = new XliffProcessor(xliff);
         String filterName = processor.getFilter();
+        if (filterName == null) {
+            LOGGER.warn("Missing filter class name in XLIFF: using DefaultFilter");
+            filterName = DefaultFilter.class.getCanonicalName();
+        }
         IFilter filter;
         try {
-            filter = (IFilter) Class.forName(filterName).newInstance();
+            Class filterClass = Class.forName(filterName);
+            if (!filterClass.equals(DefaultFilter.class)) {
+                LOGGER.info("Using custom filter: " + filterName);
+            }
+            filter = (IFilter) filterClass.newInstance();
         } catch (Exception e) {
             throw new RuntimeException("Exception while loading filter class "+ filterName, e);
         }
