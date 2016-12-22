462a15bd4136858606b7630491f0a40bcce5a09e
Giuseppe Silvano <giuseppe.silvano@gmail.com>
2016-12-22 12:33:20 +0100

    Shutdown gracefully with SIGTERM

--- filters/src/main/java/com/matecat/converter/Main.java
+++ filters/src/main/java/com/matecat/converter/Main.java
@@ -1,33 +1,27 @@
 package com.matecat.converter;
 
 import com.matecat.converter.server.MatecatConverterServer;
+import com.matecat.filters.basefilters.FiltersRouter;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
 
 public class Main {
 
-	/**
-	 * Entry point for the convert, which executes the server
-	 */
-	public static void main(String[] args) throws Exception {
+	private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
+
+	public static void main(String[] args) throws Exception {
 		// Init the server
 		MatecatConverterServer server = new MatecatConverterServer();
 
-		// Wait until the user finishes the execution
-		Scanner sc = new Scanner(System.in);
-		String command;
-		do {
-			command = sc.nextLine();
-		}
-		while (!command.toLowerCase().equals("quit")  &&  !command.toLowerCase().equals("exit"));
-
-		// Stop the server
-		server.stop();
-        LoggerFactory.getLogger(Main.class).info("[GLOBAL] The user has manually stopped the server");
-		System.out.println("\nGood bye!");
-
+		// Shutdown gracefully when receiving SIGTERM or similar
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			LOGGER.info("Shutdown signal received, stopping the server...");
+            server.stop();
+            LOGGER.info("Server stopped successfully. Good bye!");
+        }));
 	}
 
 }
